87c3f8aeef8548e129fb93affab0da5f38d65eb3
Tomasz <tomasz.exchange.place@gmail.com>
2023-10-16 17:24:39 +0200

    ISSUE-8: Add artificial sleep

--- ex3/main.tf
+++ ex3/main.tf
@@ -108,7 +108,11 @@ resource "null_resource" "provision" {
     private_key = file(local.private_key_filename)
   }
 
-  # Provision enginx
+  # Artificial sleep: TODO: Handle this in more robust way
+    provisioner "local-exec" {
+      command = "sleep 60"
+
+    }
 
     provisioner "local-exec" {
       command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u $USER -i '${openstack_networking_floatingip_v2.nginxnode.address},' --private-key $PRIVKEY $PLAYBOOK"
